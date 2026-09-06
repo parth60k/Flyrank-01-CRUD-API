8087ea4ad0112e0dcb8ee6d0d9cf5e89f94c30e5
parth60k <parthmane140@gmail.com>
2026-09-06 20:34:20 +0530

    Stage 0: create SQLite DB

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require("express");
+const Database = require("better-sqlite3");
+
+const db= new Database("tasks.db");
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        done BOOLEAN NOT NULL DEFAULT 0
+    )
+`).run();
+
+const taskCount= db.prepare("SELECT COUNT(*) AS count FROM tasks").get();
+
+if(taskCount.count===0){
+    const insert= db.prepare(`
+        INSERT INTO tasks (title,done)
+        VALUES (?,?)
+        `);
+
+        insert.run("Learn Express",0);
+        insert.run("Build CRUD API",0);
+        insert.run("Learn SQLite",0);
+}
+
 const swaggerUi=require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 

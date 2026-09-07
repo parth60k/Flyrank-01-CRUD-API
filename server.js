94b2ae363d53aac8d9538b2f6ab4301e24b4f10e
parth60k <parthmane140@gmail.com>
2026-09-07 23:05:11 +0530

    Stage 1: connect to PostgreSQL

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log(process.env.DB_HOST);
 const express = require("express");
 const Database = require("better-sqlite3");
 
@@ -10,6 +12,24 @@ db.prepare(`
         done BOOLEAN NOT NULL DEFAULT 0
     )
 `).run();
+
+const {Pool}=require("pg");
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
+});
+
+pool.query("SELECT NOW()", (err, result) => {
+    if (err) {
+        console.error("Database connection failed:", err);
+    } else {
+        console.log("Database connected:", result.rows[0]);
+    }
+});
 
 const taskCount= db.prepare("SELECT COUNT(*) AS count FROM tasks").get();
 
